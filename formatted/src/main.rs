73f81562eb1a4855b7c8cc4a558f494f2c4f906e
Shakib448 <shakiba448@gmail.com>
2025-01-10 07:18:41 +0600

    Rust Display

--- formatted/src/main.rs
+++ formatted/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 /*
 Printing is handle by series of macros defined in std::fmt some of which are:
 * format!: Write formatted text to String
@@ -40,6 +42,7 @@ fn main() {
 
     println!("{number:0>width$}");
     dbg_fn();
+    display_fn();
 }
 
 /*
@@ -83,4 +86,55 @@ fn dbg_fn() {
     let peter = Person { name, age };
 
     println!("{:#?}", peter);
+}
+
+/*
+    fmt::Debug hardly looks compact and clean, so it is often advantageous to customize the output appearance. This is done by manually implementing
+    fmt::Display, which uses the {} print marker. Implementing it looks like this:
+*/
+impl fmt::Display for Structure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+fn display_fn () {
+    let minmax = MinMax(0,14);
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300,300);
+    let small_range = MinMax(-3, 3);
+    println!("The big range is {big} and the small is {small}",small=small_range,big=big_range);
+    let point = Point2D { x: 3.3, y: 7.2 };
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
 }
