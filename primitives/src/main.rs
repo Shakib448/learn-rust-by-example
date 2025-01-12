def52b4687a74d306f0cd87e4ff5d845bdd1b496
Shakib448 <shakiba448@gmail.com>
2025-01-12 17:03:54 +0600

    RUST premitves types Done

--- primitives/src/main.rs
+++ primitives/src/main.rs
@@ -85,6 +85,98 @@ fn literals_fn() {
     println!("One million is written as {}", 1_000_000u32);
 }
 
+/*
+    Tuples
+    A tuple is a collection of values of different types. Tuples are constructed using parentheses (), and
+    each tuple itself is a value with type signature (T1,T2, ...). where T1, T2 are the types of it's members.
+    Functions can use tuples to return multiple values, as tuples can hols any number of values.
+*/
+
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (integer, boolean) = pair;
+    (boolean, integer)
+}
+
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+fn tuple_fn() {
+    let long_tuple = (1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true);
+
+    println!("Long tuple first value: {}", long_tuple.0);
+    println!("Long tuple second value: {}", long_tuple.1);
+
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    let pair = (1, true);
+
+    println!("pair is {:?}", pair);
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    println!("one element tuple: {:?}", (5u32,));
+    println!("just an integer: {:?}", (5u32));
+
+    let tuple = (1, "hello", 4.5, true);
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+}
 
 
 
+/*
+   Arrays and Slices
+
+   An array is a collection of objects of the same type T, stored in contiguous memory. Arrays are created using brackets [], and their length,
+   which is known at compile time, is part of their type signature [T; length].
+
+   Slices are similar to arrays, but their length is not known at compile time. Instead, a slice is a two-world object; the first word is a pointer to the data,
+   the second word is the length of the slice. The word size is the same as usize, determined by the processor architecture, e.g. 64 bits on an x86-64.
+   Slices can be used to borrow a section of an array and have the type signature &[T].
+*/
+
+
+fn analyze_slice(slice: &[i32]) {
+    println!("first element of the slice: {}", slice[0]);
+    println!("the slice has {} elements", slice.len());
+}
+
+fn slices_fn() {
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+    let ys: [i32; 500] = [0; 500];
+    println!("first element of the array: {}", xs[0]);
+    println!("second element of the array: {}", xs[1]);
+    println!("array size: {}", xs.len());
+    // Arrays are stack allocated
+    println!("array occupies {} bytes", mem::size_of_val(&xs));
+    println!("borrow the whole array as a slice");
+
+    // Arrays can be automatically borrowed as slices.
+    println!("Borrow the whole array as a slice.");
+    analyze_slice(&xs);
+
+    println!("borrow a section of the array as a slice");
+    analyze_slice(&ys[1..4]);
+
+    let empty_array: [u32; 0] = [];
+
+    assert_eq!(&empty_array, &[]);
+    assert_eq!(&empty_array, &[][..]);
+
+
+    // Arrays can be safely accessed using `.get`, which returns an
+    // `Option`. This can be matched as shown below, or used with
+    // `.expect()` if you would like the program to exit with a nice
+    // message instead of happily continue.
+
+    for i in 0..xs.len() + 1 {
+        match xs.get(i) {
+            Some(x) => println!("The element at index {} is {}", i, x),
+            None => println!("No element at index {}", i)
+        }
+    }
+}
