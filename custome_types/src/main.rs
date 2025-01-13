62926af7deeb995721e9db87cb434f3ac85be2a7
Shakib448 <shakiba448@gmail.com>
2025-01-13 10:07:55 +0600

    RUST custome types

--- custome_types/src/main.rs
+++ custome_types/src/main.rs
@@ -204,3 +204,88 @@ fn c_fn() {
     println!("roses are #{:06x}", Color::Red as i32);
     println!("violets are #{:06x}", Color::Blue as i32);
 }
+
+/*
+    Testcase: linked-list
+    A common way to implement a linked-list is via enums:
+*/
+
+use crate::List::*;
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+impl List {
+    fn new() -> List {
+        Nil
+    }
+
+    fn prepend(self, elem: i32) -> List {
+        Cons(elem, Box::new(self))
+    }
+
+    fn len(&self) -> u32 {
+        // `self` has to be matched, because the behavior of this method
+        // depends on the variant of `self`
+        // `self` has type `&List`, and `*self` has type `List`, matching on a
+        // concrete type `T` is preferred over a match on a reference `&T`
+        // after Rust 2018 you can use self here and tail (with no ref) below as well,
+        // rust will infer &s and ref tail.
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0,
+        }
+    }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                // `format!` is similar to `print!`, but returns a heap
+                // allocated string instead of printing to the console
+                format!("{}, {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            }
+        }
+    }
+}
+
+fn linked_fn () {
+    let mut list = List::new();
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
+}
+
+/*
+    constant
+    Rust has two different types of constants which can be declared in any scope including global.
+    Both require type annotation:
+
+    const : An unchangeable value (the common case).
+    static: A possibly mutable variable with 'static lifetime. The static lifetime is inferred and does not have to be specified.
+    Accessing or modifying a mutable static variable is unsafe.
+
+*/
+
+static LANGUAGE: &str = "Rust";
+const THRESHOLD: i32 = 10;
+
+fn is_big(n: i32) -> bool {
+    n > THRESHOLD
+}
+
+fn cons_fn() {
+    let n = 16;
+
+    println!("This is {}", LANGUAGE);
+    println!("The threshold is {}", THRESHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else { "small" });
+
+    THRESHOLD = 5
+}
