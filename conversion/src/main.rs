57bb1b551c958362c13c1a4852e85529a983247d
Shakib448 <shakiba448@gmail.com>
2025-01-14 15:36:12 +0600

    Rust conversion and express

--- conversion/src/main.rs
+++ conversion/src/main.rs
@@ -87,14 +87,13 @@ necessary. Note, however, that the converse is not true: implementing Into for y
 */
 
 
-
 #[derive(Debug, PratialEq)]
 struct EvenNumber(i32);
 
 impl TryFrom<i32> for EvenNumber {
     type Error = ();
 
-    fn try_from(value : i32) -> Result<Self, Self::Error> {
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
         if value % 2 == 0 {
             Ok(EvenNumber(value))
         } else {
@@ -103,8 +102,7 @@ impl TryFrom<i32> for EvenNumber {
     }
 }
 
-fn tryfrom_fn () {
-
+fn tryfrom_fn() {
     assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
     assert_eq!(EvenNumber::try_from(5), Err(()));
 
@@ -135,7 +133,7 @@ impl fmt::Display for Circle {
     }
 }
 
-fn strings_fn () {
+fn strings_fn() {
     let circle = Circle { radius: 3 };
     println!("{}", circle.to_string());
 }
@@ -153,7 +151,7 @@ this will convert the string into type specifed as long as the FromStr trait is 
 
 */
 
-fn parsing_fn () {
+fn parsing_fn() {
     let parsed: i32 = "5".parse().unwrap();
     let turbo_parsed = "10".parse::<i32>().unwrap();
 
@@ -176,9 +174,53 @@ impl FromStr for Circle {
 }
 
 
-fn fn_prasing () {
+fn fn_prasing() {
     let radius = "     3 ";
-    let circle : Circle = radius.parse().unwrap();
+    let circle: Circle = radius.parse().unwrap();
 
     println!("{:?}", circle);
+}
+
+
+// Expressions
+
+// A Rust program is (mostly) made up of a series of statements:
+
+fn expressions() {
+    // statement
+    // statement
+    // statement
+}
+
+// There are a few kinds of statements in Rust. The most common two are declaring a variable binding, and using a ; with an expression:
+fn expressions_1() {
+    let x = 5;
+    x;
+    // expression;
+    x + 1;
+    15
+}
+/*
+Blocks are expressions too, so they can be used as values in assignments.
+ The last expression in the block will be assigned to the place expression such as a local variable.
+ However, if the last expression of the block ends with a semicolon, the return value will be ().
+*/
+fn expressions_2() {
+
+    let x = 5u32;
+
+    let y = {
+        let x_squared = x * x;
+        let x_cube = x_squared * x;
+        // This expression will be assigned to `y`
+        x_cube + x_squared + x
+    };
+
+
+    let z = {
+        // The semicolon suppresses this expression and `()` is assigned to `z`
+        2 * x;
+    };
+
+    println!("x: {}, y: {}, z: {}", x, y, z);
 }
