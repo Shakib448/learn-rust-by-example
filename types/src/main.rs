b14e1da3b85dce914dd59c787471cb049d09ce52
Shakib448 <shakiba448@gmail.com>
2025-01-15 20:12:07 +0600

    RUST destructuring

--- types/src/main.rs
+++ types/src/main.rs
@@ -136,12 +136,12 @@ fn inference_fn() {
 
 /*
 Aliasing
-The type statement can be used to give a new name to an existing type. Types must have UpperCamelCase names, or the compiler will raise a warning.
+The type statement can be used to give a logical name to an existing type. Types must have UpperCamelCase names, or the compiler will raise a warning.
  The exception to this rule are the primitive types: usize, f32, etc.
 */
 
 fn aliasing_fn() {
-    // `NanoSecond`, `Inch`, and `U64` are new names for `u64`.
+    // `NanoSecond`, `Inch`, and `U64` are logical names for `u64`.
     type NanoSecond = u64;
     type Inch = u64;
     type U64 = u64;
@@ -152,7 +152,7 @@ fn aliasing_fn() {
         let inches: Inch = 2 as U64;
 
         // Note that type aliases *don't* provide any extra type safety, because
-        // aliases are *not* new types
+        // aliases are *not* logical types
         println!("{} nanoseconds + {} inches = {} unit?",
                  nanoseconds,
                  inches,
