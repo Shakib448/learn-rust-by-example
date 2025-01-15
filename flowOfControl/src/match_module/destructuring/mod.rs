866988601c917caaab5402a1bfac1fd9006b4ea5
Shakib448 <shakiba448@gmail.com>
2025-01-15 15:53:20 +0600

    RUST destructuring

--- flowOfControl/src/match_module/destructuring/mod.rs
+++ flowOfControl/src/match_module/destructuring/mod.rs
@@ -3,9 +3,11 @@ mod tuples_exp;
 mod array_slices;
 mod enum_exp;
 mod pointers;
+mod structs_exp;
 pub fn run () {
     tuples_exp::run();
     array_slices::run();
     enum_exp::run();
     pointers::run();
+    structs_exp::run();
 }
