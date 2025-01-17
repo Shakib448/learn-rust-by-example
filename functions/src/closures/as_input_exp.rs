770d76fe9fa63cdaabfef84331ebe66ddb63be5c
Shakib448 <shakiba448@gmail.com>
2025-01-17 07:20:46 +0600

    RUST closure as input parameters

--- functions/src/closures/as_input_exp.rs
+++ functions/src/closures/as_input_exp.rs
@@ -10,4 +10,15 @@ Fn: The closure uses the captured value by reference (&T)
 FnMut: The closure uses the captured value by mutable reference (&mut T)
 FnOnce: The closure uses the captured value by value (T)
 
+On a variable-by-variable basis, the compiler will capture variables in the least restrictive manner possible
+
+For instance, consider a parameter annotated as FnOnce. This specifies that the closure may capture
+by &T, &mut T or T, but the compiler will ultimately chooses based on how the captured variables
+are used in the closure.
+
+This is because if a move is possible, then any type of borrow should also be possible. Note that the
+reverse in not rue. If the parameter is annotated as Fn, then capturing variables by &mut T or T are
+not allowed. However. &T is allowed
+
+In the following example, try swapping the usage of FN, FnMut and FnOnce to see what happens
 */
