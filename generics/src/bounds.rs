90572e05c48813e4facba7621e6627b94f29a448
Shakib448 <shakiba448@gmail.com>
2025-01-23 10:32:01 +0600

    RUST bounds added

---
+++ generics/src/bounds.rs
@@ -0,0 +1,49 @@
+/*
+Bounds
+When working with generics, the type parameters often must use traits as bounds
+to stipulate what functionality a type implements. For example, the following example use
+the trait Display to print and so it requires T to be bound by Display; that is, T must implement
+Display.
+*/
+
+fn printer<T: Display>(t: T) {
+    println!("{}", t);
+}
+
+struct S<T: Display>(T);
+// let s = S(vec![1]);
+
+use std::fmt::Debug;
+
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+impl HasArea for Rectangle {
+    fn area(&self) -> f64 { self.length * self.height }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    length: f64,
+    height: f64,
+}
+#[allow(dead_code)]
+struct Triangle {
+    length: f64,
+    height: f64,
+}
+
+fn print_debug<T: Debug>(t: &T) {
+    println!("{:?}", t);
+}
+
+fn area<T: HasArea>(t: &T) -> f64 { t.area() }
+
+fn main() {
+    let rectangle = Rectangle { length: 3.0, height: 4.0 };
+    let _triangle = Triangle { length: 6.0, height: 5.0 };
+
+    print_debug(&rectangle);
+    println!("Area: {}", area(&rectangle));
+}
