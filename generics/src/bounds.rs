05eb432fc29fe91ca88892f6ed815187caf71841
Shakib448 <shakiba448@gmail.com>
2025-01-23 10:36:39 +0600

    RUST bounds added

--- generics/src/bounds.rs
+++ generics/src/bounds.rs
@@ -47,3 +47,32 @@ fn main() {
     print_debug(&rectangle);
     println!("Area: {}", area(&rectangle));
 }
+
+/*
+Testcase: empty bounds
+A consequence of how bounds work is that even if a trait doesn't include any
+functionality, you can still use it as a bound. Eq and Copy are examples of such trait
+s from the std library.
+*/
+
+struct Cardinal;
+struct BlueJay;
+struct Turkey;
+
+trait Red {}
+trait Blue {}
+
+impl Red for Cardinal {}
+impl Blue for BlueJay {}
+
+fn red<T: Red>(_: &T) -> &'static str { "red" }
+fn blue<T: Blue>(_: &T) -> &'static str { "blue" }
+
+fn run1() {
+    let cardinal = Cardinal;
+    let blue_jay = BlueJay;
+    let _turkey = Turkey;
+
+    println!("A cardinal is {}", red(&cardinal));
+    println!("A blue jay is {}", blue(&blue_jay));
+}
