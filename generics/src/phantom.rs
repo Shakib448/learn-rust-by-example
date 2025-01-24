f651a3cf578d824dba09b234d1fa1bf2bbce051a
Shakib448 <shakiba448@gmail.com>
2025-01-24 11:12:22 +0600

    RUST phantomdata

--- generics/src/phantom.rs
+++ generics/src/phantom.rs
@@ -39,3 +39,49 @@ fn main() {
         phantom: PhantomData,
     };
 }
+
+/*
+Testcase: unit clarification
+
+A useful method of unit conversions can be examined by
+implementing Add with a phantom type parameter. The Add trait
+is examined below:
+*/
+
+
+pub trait Add<RHS = Self> {
+    type Output;
+    fn add(self, rhs: RHS) -> Self::Output;
+}
+
+impl<U> Add for T<U> {
+    type Output = T<U>;
+}
+
+use std::marker::PhantomData;
+use std::ops::Add;
+
+#[derive(Debug, Clone, Copy)]
+enum Inch {}
+#[derive(Debug, Clone, Copy)]
+enum Mm {}
+
+#[derive(Debug, Clone, Copy)]
+struct Length<Unit>(f64, PhantomData<Unit>);
+
+impl<Unit> Add for Length<Unit> {
+    type Output = Length<Unit>;
+    fn add(self, rhs: Length<Unit>) -> Length<Unit> {
+        Length(self.0 + rhs.0, PhantomData)
+    }
+}
+
+fn run1() {
+    let one_foot: Length<Inch> = Length(12.0, PhantomData);
+    let one_meter: Length<Mm> = Length(1000.0, PhantomData);
+
+    let two_feet = one_foot + one_foot;
+    let two_meters = one_meter + one_meter;
+    println!("one foot + one_foot = {:?} in", two_feet.0);
+    println!("one meter + one_meter = {:?} mm", two_meters.0);
+}
