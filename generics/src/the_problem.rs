b0f1b0d5b3ce4f7ee3e8ae2853d689ff0f53c16a
Shakib448 <shakiba448@gmail.com>
2025-01-24 10:59:28 +0600

    RUST the problem

--- generics/src/the_problem.rs
+++ generics/src/the_problem.rs
@@ -54,3 +54,62 @@ fn main() {
     println!("Last number: {}", container.last());
     println!("The difference is {}", difference(&container));
 }
+
+/*
+Associated types
+The use of "Associated types" improves the overall readability of code by
+moving inner types locally into a trait as output types. Syntax for the
+trait definition is a follows
+*/
+
+trait Contains {
+    type A;
+    type B;
+
+    fn contains(&self, _: &Self::A, _: &Self::B) -> bool;
+}
+
+fn difference<A, B, C>(container: &C) -> i32
+where
+    C: Contains<A, B>,
+{ ... }
+
+fn difference<C: Contains>(container: &C) -> i32 { ... }
+
+
+struct Container(i32, i32);
+
+trait Contains {
+    type A;
+    type B;
+
+    fn contains(&self, _: &Self::A, _: &Self::B) -> bool;
+    fn first(&self) -> i32;
+    fn last(&self) -> i32;
+}
+
+impl Contains for Container {
+    type A = i32;
+    type B = i32;
+
+    fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
+        (&self.0 == number_1) && (&self.1 == number_2)
+    }
+    fn first(&self) -> i32 { self.0 }
+    fn last(&self) -> i32 { self.1 }
+}
+
+fn difference<C: Contains>(container: &C) -> i32 {
+    container.last() - container.first()
+}
+
+fn run1() {
+    let number_1 = 3;
+    let number_2 = 10;
+    let container = Container(number_1, number_2);
+    println!("Does container contain {} and {}: {}", &number_1, &number_2, &container.contains(&number_1, &number_2));
+
+    println!("First number: {}", container.first());
+    println!("Last number: {}", container.last());
+    println!("The difference is {}", difference(&container));
+}
