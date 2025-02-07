be7c555afb83d9845f195a8106cec8845292c0b5
Shakib448 <shakiba448@gmail.com>
2025-02-07 22:07:51 +0600

    WIP on macro rules

---
+++ macro_rules/src/overload.rs
@@ -0,0 +1,30 @@
+/*
+Overload
+Macros can be overloaded to accept different combinations of arguments.
+ In that regard, macro_rules! can work similarly to a match block:
+*/
+
+macro_rules! test {
+    ($left:expr; and $right:expr) => {
+        println!(
+            "{:?} and {:?} is {:?}",
+            stringify!($left),
+            stringify!($right),
+            $left && $right
+        )
+    };
+
+    ($left:expr; or $right:expr) => {
+        println!(
+            "{:?} or {:?} is {:?}",
+            stringify!($left),
+            stringify!($right),
+            $left || $right
+        )
+    };
+}
+
+fn main() {
+    test!(1i32 + 1 == 2i32; and 2i32 * 2 == 4i32);
+    test!(true; or false);
+}
