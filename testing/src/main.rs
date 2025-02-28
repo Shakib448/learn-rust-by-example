aa4dfebaed2a80d5e9d51918ba9d5cf2009926ce
Shakib448 <shakiba448@gmail.com>
2025-02-28 09:28:27 +0600

    RUST unit testing

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -41,3 +41,67 @@ mod tests {
         assert_eq!(bad_add(1, 2), 3);
     }
 }
+
+
+/*
+Tests and ?
+None of the previous unit test examples had a return type. But in Rust 2018, your unit tests can return Result<()>, which lets you use ? in them! This can make them much more concise.
+*/
+fn sqrt(number: f64) -> Result<f64, String> {
+    if number >= 0.0 {
+        Ok(number.powf(0.5))
+    } else {
+        Err("negative floats don't have square roots".to_owned())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sqrt() -> Result<(), String> {
+        let x = 4.0;
+        assert_eq!(sqrt(x)?.powf(2.0), x);
+        Ok(())
+    }
+}
+
+/*
+Testing panics
+To check functions that should panic under certain circumstances, use attribute #[should_panic].
+This attribute accepts optional parameter expected = with the text of the panic message.
+If your function can panic in multiple ways, it helps make sure your test is testing the correct panic.
+*/
+
+
+pub fn divide_non_zero_result(a: u32, b: u32) -> u32 {
+    if b == 0 {
+        panic!("Divide-by-zero error");
+    } else if a < b {
+        panic!("Divide result is zero");
+    }
+    a / b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_divide() {
+        assert_eq!(divide_non_zero_result(10, 2), 5);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_any_panic() {
+        divide_non_zero_result(1, 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "Divide result is zero")]
+    fn test_specific_panic() {
+        divide_non_zero_result(1, 10);
+    }
+}
