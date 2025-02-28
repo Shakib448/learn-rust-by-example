d9c903e9f01e4bde7664673eb5b5e86d9edaf477
Shakib448 <shakiba448@gmail.com>
2025-02-28 09:28:56 +0600

    RUST unit testing

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -105,3 +105,35 @@ mod tests {
         divide_non_zero_result(1, 10);
     }
 }
+
+
+/*
+Ignoring tests
+Tests can be marked with the #[ignore] attribute to exclude some tests. Or to run them with command cargo test -- --ignored
+*/
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 2), 4);
+    }
+
+    #[test]
+    fn test_add_hundred() {
+        assert_eq!(add(100, 2), 102);
+        assert_eq!(add(2, 100), 102);
+    }
+
+    #[test]
+    #[ignore]
+    fn ignored_test() {
+        assert_eq!(add(0, 0), 0);
+    }
+}
